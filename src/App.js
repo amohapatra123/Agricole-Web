698675b56621e038878d3c9cf98de6ee6b915e07
Anshuman Mohapatra <amohapatra2001@gmail.com>
2021-02-27 12:36:51 +0530

    initial setup commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Loader from "./components/loader";
 import { routes } from "./routes/routes";
-import { Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
-import { NoteProvider } from "./context/noteContext";
-import { ShareProvider } from "./context/shareContext";
+
 function App() {
   const routcomponent = routes.map(({ path, component, id }) => (
     <Route exact path={path} component={component} key={id} />
@@ -15,47 +12,7 @@ function App() {
     <>
       <Suspense fallback={<Loader />}>
         <Router>
-          <Row>
-            <Col md={{ size: 2 }} id="sidebar">
-              <aside>
-                <ul>
-                  <Link to="/">
-                    <li id="highlight">
-                      <h3>
-                        <svg
-                          width="36"
-                          height="36"
-                          viewBox="0 0 36 36"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M20 12V0H36V12H20ZM36 36H20V16H36V36ZM0 36H16V24H0V36ZM16 20H0V0H16V20Z"
-                            fill="white"
-                          />
-                        </svg>
-                        Dashboard
-                      </h3>
-                    </li>
-                  </Link>
-                  <Link to="/page2">
-                    <li>
-                      <h3>Page2</h3>
-                    </li>
-                  </Link>
-                </ul>
-              </aside>
-            </Col>
-            <Col>
-              <Switch>
-                <ShareProvider>
-                  <NoteProvider>{routcomponent}</NoteProvider>
-                </ShareProvider>
-              </Switch>
-            </Col>
-          </Row>
+          <Switch>{routcomponent}</Switch>
         </Router>
       </Suspense>
     </>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,4 @@
 import React from "react";
-const Homepage = React.lazy(() => import("../container/page1.js"));
-const Page2 = React.lazy(() => import("../container/page2.js"));
-export const routes = [
-  { id: 0, path: "/", component: Homepage },
-  { id: 1, path: "/page2", component: Page2 },
-];
+const Homepage = React.lazy(() => import("../container/homepage.js"));
+
+export const routes = [{ id: 0, path: "/", component: Homepage }];
