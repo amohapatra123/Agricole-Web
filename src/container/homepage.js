4b88bfccd895ea8d25b0f0093f9cff15e620ae11
Anshuman Mohapatra <amohapatra2001@gmail.com>
2021-02-27 14:01:37 +0530

    homepage ready

--- src/container/homepage.js
+++ src/container/homepage.js
@@ -1,5 +1,22 @@
 import React from "react";
-
+import "../styles/home.css";
+import { Row, Col } from "reactstrap";
 export default function homepage() {
-  return <div>This is the homepage</div>;
+  return (
+    <Row className="main-div">
+      <Col xs={{ size: 12 }} md={{ size: 6 }} className="login-div">
+        <div className="title">Agricole</div>
+        <div className="question">Are you a ?</div>
+        <Row className="buttons">
+          <Col xs={{ size: 12 }} md={{ size: 3 }} className="shop">
+            Shopkeeper
+          </Col>
+
+          <Col xs={{ size: 12 }} md={{ size: 3, offset: 1 }} className="user">
+            User
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyCQuxWbupmtaHC9vwJ3xhnmCvfaanm-sxY",
+  authDomain: "agricole-400f1.firebaseapp.com",
+  projectId: "agricole-400f1",
+  storageBucket: "agricole-400f1.appspot.com",
+  messagingSenderId: "1050897790965",
+  appId: "1:1050897790965:web:ea6ca4bfa5cde2ce3492bc",
+  measurementId: "G-EFJ479MV59",
+};
+
+firebase.initializeApp(config);
+export default firebase;
