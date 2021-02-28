739b4fc9fa50a22dffed77378a7cc4a7556bd3aa
Anshuman Mohapatra <amohapatra2001@gmail.com>
2021-02-28 10:13:48 +0530

    warnings ruled out

--- src/container/user/dashboard.js
+++ src/container/user/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/dash.css";
 import { Row, Col } from "reactstrap";
 import firebase from "../../firebase";
